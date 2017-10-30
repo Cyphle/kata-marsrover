55a469b4f1e139b3fd89dd585f120388eed67f99
Cyril Pham-le <cyril.phamle@gmail.com>
2017-10-30 20:38:45 +0100

    Land rover can be instantiated with xCoordinate

--- src/main/java/fr/marsrover/navigation/LandRover.java
+++ src/main/java/fr/marsrover/navigation/LandRover.java
@@ -1,4 +1,13 @@
 package fr.marsrover.navigation;
 
 public class LandRover {
+  private int xCoordinate;
+
+  public LandRover(int xCoordinate) {
+    this.xCoordinate = xCoordinate;
+  }
+
+  public int getXCoordinate() {
+    return xCoordinate;
+  }
 }
--- src/test/java/fr/marsrover/navigation/LandRoverTest.java
+++ src/test/java/fr/marsrover/navigation/LandRoverTest.java
@@ -8,8 +8,14 @@ public class LandRoverTest {
   @Test
   public void should_be_instantiable() throws Exception {
     // When
-    LandRover landRover = new LandRover();
+    LandRover landRover = new LandRover(0);
     // Then
     assertThat(landRover).isNotNull();
+  }
+
+  @Test
+  public void should_have_a_x_coordinate_when_initializing_with_x_coordinate() throws Exception {
+    LandRover landRover = new LandRover(23);
+    assertThat(landRover.getXCoordinate()).isEqualTo(23);
   }
 }
